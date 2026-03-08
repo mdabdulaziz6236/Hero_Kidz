82a6cb84f3b765271c77cc5cf29e3e69e30c5bc7
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2026-03-08 10:48:52 +0600

    Updated Navlink

--- src/components/buttons/NavLink.jsx
+++ src/components/buttons/NavLink.jsx
@@ -3,11 +3,18 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 
-const Navlink = ({href , className, children}) => {
-    const path = usePathname()
+const Navlink = ({ href, className, children }) => {
+    const path = usePathname();
+
+    // 1. Create a simple logic check
+    const isActive = path === href || (href !== '/' && path.startsWith(href));
+
     return (
-        <Link className={`${className} ${path.startsWith(href) && 'text-primary'} font-medium`} href={href}>
-        {children}
+        <Link 
+            className={`${className} ${isActive ? 'text-primary' : ''} md:text-[16px] lg:text-[18px] font-medium`} 
+            href={href}
+        >
+            {children}
         </Link>
     );
 }
