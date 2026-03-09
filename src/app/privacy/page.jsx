29ff47043749f2fb0eb065f1d2031dfba931378b
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2026-03-09 17:53:07 +0600

    Feat: Added some page

---
+++ src/app/privacy/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            Privacy
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/app/terms/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            Terms
+        </div>
+    );
+}
+
+export default Page;
